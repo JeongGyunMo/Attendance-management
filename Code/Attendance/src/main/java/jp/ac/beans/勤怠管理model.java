afbc9fc25ebd5cc5f1d4ce80312d80512ebf977a
JeongGyunMo <jmlyangl@naver.com>
2020-10-29 19:35:30 +0900

    spring boot 재설정

--- Code/Attendance/src/main/java/jp/ac/beans/勤怠管理model.java
+++
@@ -1,8 +0,0 @@
-package jp.ac.beans;
-
-import lombok.Data;
-@Data
-public class 勤怠管理model {
-	private String id;
-	private String pass;
-}
--- Code/Attendance/src/main/java/jp/ac/controller/AttendanceApplication.java
+++ Code/Attendance/src/main/java/jp/ac/controller/AttendanceApplication.java
@@ -2,13 +2,12 @@ package jp.ac.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
-@Controller
 @SpringBootApplication
 public class AttendanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceApplication.class, args);
-	}	
+	}
+
 }
