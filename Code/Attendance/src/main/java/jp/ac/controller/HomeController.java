6b67d03869588a70e35360097f1c388753b96f16
JeongGyunMo <jmlyangl@naver.com>
2020-11-05 14:59:31 +0900

    DB

--- Code/Attendance/src/main/java/jp/ac/controller/HomeController.java
+++ Code/Attendance/src/main/java/jp/ac/controller/HomeController.java
@@ -1,15 +1,24 @@
 package jp.ac.controller;
 
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.ac.beans.勤怠管理model;
+import jp.ac.entities.社員マスタRepository;
+import jp.ac.services.勤怠管理service;
  
 @Controller
 public class HomeController {
- 
+
+	private 勤怠管理service 勤怠管理service; 
+	社員マスタRepository 社員マスタRepository;
+	
     @RequestMapping("/")
     public String home() {
         return "ホーム";
@@ -17,5 +26,12 @@ public class HomeController {
 	@RequestMapping(value = "sign", method = RequestMethod.GET)
 	public String sing(Locale locale, Model model) {
 		return "新規加入";
+	}
+	
+	@PostMapping("register")
+	public String register(Model model, 勤怠管理model Account) {
+		List<勤怠管理model> list = 勤怠管理service.findall(Account);
+		model.addAttribute("勤怠管理List",list);
+		return "ホーム";
 	}
 }
--- Code/Attendance/src/main/java/jp/ac/controller/勤怠管理controller.java
+++ Code/Attendance/src/main/java/jp/ac/controller/勤怠管理controller.java
@@ -17,14 +17,7 @@ import jp.ac.entities.社員マスタ;
 @RequestMapping("勤怠管理")
 public class 勤怠管理controller {
 
-	private 勤怠管理service 勤怠管理service; 
-	社員マスタRepository 社員マスタRepository;
 	
 
-	@PostMapping("/register")
-	public void register(Model model, 勤怠管理model Account) {
-		List<勤怠管理model> list = 勤怠管理service.findall(Account);
-		model.addAttribute("勤怠管理List",list);
-		
-	}
+
 }
